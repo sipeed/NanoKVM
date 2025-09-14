00d30efa457e8b6e06967c97bedab1b88282c603
Alexander-Ger-Reich <50119493+Alexander-Ger-Reich@users.noreply.github.com>
2025-09-14 12:12:44 +0200

    Update virtual-keys.ts

--- web/src/pages/desktop/keyboard/virtual-keys.ts
+++ web/src/pages/desktop/keyboard/virtual-keys.ts
@@ -32,8 +32,8 @@ export const keyboardOptions = {
     qwertz: [
       '{escape} F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12',
       'Backquote Digit1 Digit2 Digit3 Digit4 Digit5 Digit6 Digit7 Digit8 Digit9 Digit0 Minus Equal {backspace}',
-      '{tab} KeyQ KeyW KeyE KeyR KeyT KeyZ KeyU KeyI KeyO KeyP BracketLeft BracketRight Backslash',
-      '{capslock} KeyA KeyS KeyD KeyF KeyG KeyH KeyJ KeyK KeyL Semicolon Quote {enter}',
+      '{tab} KeyQ KeyW KeyE KeyR KeyT KeyZ KeyU KeyI KeyO KeyP KeyUE BracketLeft BracketRight Backslash',
+      '{capslock} KeyA KeyS KeyD KeyF KeyG KeyH KeyJ KeyK KeyL KeyOE KeyAE Semicolon Quote {enter}',
       '{shiftleft} KeyY KeyX KeyC KeyV KeyB KeyN KeyM Comma Period Slash {shiftright}',
       '{controlleft} {winleft} {altleft} {space} {altright} {winright} {menu} {controlright}'
     ],
