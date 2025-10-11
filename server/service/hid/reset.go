817924eac13726fa8a830a251857de0b44cdb45e
scpcom <scpcom@gmx.de>
2025-10-11 18:34:04 +0200

    server: hid: split reset into disable/enable

--- server/service/hid/reset.go
+++ server/service/hid/reset.go
@@ -21,62 +21,76 @@ func (s *Service) Reset(c *gin.Context) {
 	}()
 
 	// reset USB
+	if err := disable(); err != nil {
+		log.Errorf("disable hid failed: %s", err)
+		rsp.ErrRsp(c, -1, "disable hid failed")
+		return
+	}
+
+	if err := enable(); err != nil {
+		log.Errorf("enable hid failed: %s", err)
+		rsp.ErrRsp(c, -1, "enable hid failed")
+		return
+	}
+
+	rsp.OkRsp(c)
+	log.Debugf("reset hid success")
+}
+
+func disable() (error) {
+	// reset USB
 	f, err := os.OpenFile("/sys/kernel/config/usb_gadget/g0/UDC", os.O_WRONLY, 0644)
 	if err != nil {
 		log.Errorf("open /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
-		rsp.ErrRsp(c, -1, "open usb gadget file failed")
-		return
+		return err
 	}
 	err = f.Truncate(0)
 	if err != nil {
 		_ = f.Close()
 		log.Errorf("truncate /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
-		rsp.ErrRsp(c, -1, "truncate usb gadget file failed")
-		return
+		return err
 	}
 	_, err = f.Seek(0, 0)
 	if err != nil {
 		_ = f.Close()
 		log.Errorf("seek to 0 failed: %s", err)
-		rsp.ErrRsp(c, -1, "seek to 0 in usb gadget file failed")
-		return
+		return err
 	}
 	_, err = f.WriteString("\n")
 	if err != nil {
 		_ = f.Close()
 		log.Errorf("write to /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
-		rsp.ErrRsp(c, -1, "write to usb gadget file failed")
-		return
+		return err
 	}
 	_ = f.Close()
 
 	time.Sleep(1 * time.Second)
 
+	return nil
+}
+
+func enable() (error) {
 	devices, err := os.ReadDir("/sys/class/udc/")
 	if err != nil {
 		log.Errorf("read udc directory failed: %s", err)
-		rsp.ErrRsp(c, -1, "read udc directory failed")
-		return
+		return err
 	}
 
-	f, err = os.OpenFile("/sys/kernel/config/usb_gadget/g0/UDC", os.O_WRONLY, 0644)
+	f, err := os.OpenFile("/sys/kernel/config/usb_gadget/g0/UDC", os.O_WRONLY, 0644)
 	if err != nil {
 		log.Errorf("open /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
-		rsp.ErrRsp(c, -1, "open usb gadget file failed")
-		return
+		return err
 	}
 	for _, device := range devices {
 		_, err = f.WriteString(device.Name() + "\n")
 		if err != nil {
 			_ = f.Close()
 			log.Errorf("write to /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
-			rsp.ErrRsp(c, -1, "write to usb gadget file failed")
-			return
+			return err
 		}
 	}
 
 	_ = f.Close()
 
-	rsp.OkRsp(c)
-	log.Debugf("reset hid success")
+	return nil
 }
