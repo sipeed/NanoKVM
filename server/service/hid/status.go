911a3ef253040b7f3175f5b04c1f2d5adc6144c8
scpcom <scpcom@gmx.de>
2025-10-11 18:34:04 +0200

    server: hid: split reset into disable/enable

--- server/service/hid/status.go
+++ server/service/hid/status.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 
@@ -88,6 +87,64 @@ func (s *Service) SetHidMode(c *gin.Context) {
 	_ = exec.Command("reboot").Run()
 }
 
+func disable() (error) {
+	// reset USB
+	f, err := os.OpenFile("/sys/kernel/config/usb_gadget/g0/UDC", os.O_WRONLY, 0644)
+	if err != nil {
+		log.Errorf("open /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
+		return err
+	}
+	err = f.Truncate(0)
+	if err != nil {
+		_ = f.Close()
+		log.Errorf("truncate /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
+		return err
+	}
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		_ = f.Close()
+		log.Errorf("seek to 0 failed: %s", err)
+		return err
+	}
+	_, err = f.WriteString("\n")
+	if err != nil {
+		_ = f.Close()
+		log.Errorf("write to /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
+		return err
+	}
+	_ = f.Close()
+
+	time.Sleep(1 * time.Second)
+
+	return nil
+}
+
+func enable() (error) {
+	devices, err := os.ReadDir("/sys/class/udc/")
+	if err != nil {
+		log.Errorf("read udc directory failed: %s", err)
+		return err
+	}
+
+	f, err := os.OpenFile("/sys/kernel/config/usb_gadget/g0/UDC", os.O_WRONLY, 0644)
+	if err != nil {
+		log.Errorf("open /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
+		return err
+	}
+	for _, device := range devices {
+		_, err = f.WriteString(device.Name() + "\n")
+		if err != nil {
+			_ = f.Close()
+			log.Errorf("write to /sys/kernel/config/usb_gadget/g0/UDC failed: %s", err)
+			return err
+		}
+	}
+
+	_ = f.Close()
+
+	return nil
+}
+
 func (s *Service) ResetHid(c *gin.Context) {
 	var rsp proto.Response
 
@@ -99,11 +156,16 @@ func (s *Service) ResetHid(c *gin.Context) {
 		h.Unlock()
 	}()
 
-	command := fmt.Sprintf("%s restart_phy", USBDevScript)
-	err := exec.Command("sh", "-c", command).Run()
-	if err != nil {
-		log.Errorf("failed to reset hid: %v", err)
-		rsp.ErrRsp(c, -1, "failed to reset hid")
+	// reset USB
+	if err := disable(); err != nil {
+		log.Errorf("disable hid failed: %s", err)
+		rsp.ErrRsp(c, -1, "disable hid failed")
+		return
+	}
+
+	if err := enable(); err != nil {
+		log.Errorf("enable hid failed: %s", err)
+		rsp.ErrRsp(c, -1, "enable hid failed")
 		return
 	}
 
