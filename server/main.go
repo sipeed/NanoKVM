2ba74a1884891d927ff3670d12136e6259cc2ecc
Kirill Nikiforov <me@allmazz.me>
2026-03-24 22:02:05 +0400

    add `host` server config parameter

--- server/main.go
+++ server/main.go
@@ -69,10 +69,11 @@ func run() {
 
 	router.Init(r)
 
-	httpAddr := fmt.Sprintf(":%d", conf.Port.Http)
-	httpsAddr := fmt.Sprintf(":%d", conf.Port.Https)
+	httpAddr := fmt.Sprintf("%s:%d", conf.Host, conf.Port.Http)
 
 	if conf.Proto == "https" {
+		httpsAddr := fmt.Sprintf("%s:%d", conf.Host, conf.Port.Https)
+
 		go func() {
 			r.Use(middleware.Tls())
 			err := r.RunTLS(httpsAddr, conf.Cert.Crt, conf.Cert.Key)
